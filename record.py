5250b719acb6e8fd2c197420da9895825a0759a0
Research Development Team <112861917+Research-Development5@users.noreply.github.com>
2022-11-11 09:18:12 +0500

    Update record.py

--- record.py
+++ record.py
@@ -1,13 +1,49 @@
-import sounddevice as sd
-import wavio
-def record(duration=5, fs=48000):
-    sd.default.samplerate = fs
-    sd.default.channels = 1
-    myrecording = sd.rec(int(duration * fs))
-    sd.wait(duration)
-    return myrecording
+#import sounddevice as sd
+#import wavio
+
+
+
+import streamlit as st
+#from audiorecorder import audiorecorder
+
+
+
+
+
+def record(duration, fs):
+  #  sd.default.samplerate = fs
+  #  sd.default.channels = 1
+    
+    
+   # st.title("Audio Recorder")
+   # audio1 = audiorecorder("Click to record", "Recording...")
+    st.write(audio1)
+    if len(audio1) > 0:
+    	st.audio(audio1)
+    	st.write("آواز دوبارہ ریکارڈ کرنے کے لیے' ریکارڈ کیجیے' کا بٹن دبایں۔")
+    	wav_file = open("/home/amad/streamlit/voice recording (AADIL)/temp/sample1.wav", "wb")
+    	wav_file.write(audio1.tobytes())
+
+
+
+
+
+   # myrecording = sd.rec(int(duration * fs))
+   # sd.wait(duration)
+   # return myrecording
+    return audio1
 def save_record(path_myrecording, myrecording, fs):
-    wavio.write(path_myrecording, myrecording, fs, sampwidth=2)
+
+  #  wavio.write(path_myrecording, myrecording, fs, sampwidth=2)
+    
+
+    
+	
+  
+    print("myrecording")
+    	
+    
+    
     return None
 def read_audio(file):
     with open(file, "rb") as audio_file:
